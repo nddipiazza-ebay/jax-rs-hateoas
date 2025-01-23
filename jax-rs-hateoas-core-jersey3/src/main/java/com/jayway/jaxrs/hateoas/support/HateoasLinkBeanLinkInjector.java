1ec37ffcf39b557da23770dbac3a99aaa1884260
ndipiazza <ndipiazza@ebay.com>
2025-01-23 12:35:19 -0600

    rename artifactId to newer

---
+++ jax-rs-hateoas-core-jersey3/src/main/java/com/jayway/jaxrs/hateoas/support/HateoasLinkBeanLinkInjector.java
@@ -0,0 +1,36 @@
+package com.jayway.jaxrs.hateoas.support;
+
+import com.google.common.collect.Collections2;
+import com.jayway.jaxrs.hateoas.HateoasLinkBean;
+import com.jayway.jaxrs.hateoas.HateoasLinkInjector;
+import com.jayway.jaxrs.hateoas.HateoasVerbosity;
+import com.jayway.jaxrs.hateoas.LinkProducer;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kallestenflo
+ * Date: 2/27/12
+ * Time: 6:06 PM
+ */
+public class HateoasLinkBeanLinkInjector implements HateoasLinkInjector<Object> {
+    @Override
+    public boolean canInject(Object entity) {
+        return (entity instanceof HateoasLinkBean);
+    }
+
+    @Override
+    public Object injectLinks(Object entity, LinkProducer<Object> linkProducer, final HateoasVerbosity verbosity) {
+
+        HateoasLinkBean linkBean = (HateoasLinkBean) entity;
+
+        Collection<Map<String,Object>> links = Collections2.transform(linkProducer.getLinks(entity),
+				from -> from.toMap(verbosity));
+
+        linkBean.setLinks(links);
+
+        return linkBean;
+    }
+}
---
+++ jax-rs-hateoas-core-jersey3/src/test/java/com/jayway/jaxrs/hateoas/DummyAnnotatedClass.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2011 the original author or authors.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jayway.jaxrs.hateoas;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/dummy")
+public class DummyAnnotatedClass {
+
+	@PUT
+	@Linkable(value = "test.dummy.fully.documented", description = "test description", label = "test label", templateClass = DummyDto.class)
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@Produces({ MediaType.TEXT_HTML, MediaType.TEXT_PLAIN })
+	public Response fullyDocumented(DummyDto input) {
+		return null;
+	}
+
+	@GET
+	@Linkable(value = "test.dummy.get")
+	public Response get() {
+		return null;
+	}
+
+	@GET
+	@Linkable(value = "test.dummy.get.subpath")
+	@Path("/subpath")
+	public Response getSubpath() {
+		return null;
+	}
+
+	@POST
+	@Linkable(value = "test.dummy.post")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response post(DummyDto input) {
+		return null;
+	}
+
+	@DELETE
+	@Linkable(value = "test.dummy.delete")
+	public Response delete(DummyDto input) {
+		return null;
+	}
+}
